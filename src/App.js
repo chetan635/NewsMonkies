cfc43aef0ef407dfb7592465e67be8a00ea9995e
chetan635 <68587552+chetan635@users.noreply.github.com>
2021-10-27 16:06:57 +0530

    This is My News App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar/>
+          {/* <News pageSize={5} category="science" country="in"/> */}
+
+          {/* switch */}
+          <Switch>
+          <Route exact path="/">
+              <News key="general" pageSize={5} category="general" country="in"/>
+          </Route>
+          <Route exact path="/business">
+              <News key="business" pageSize={5} category="business" country="in"/>
+          </Route>
+          <Route exact path="/entertainment">
+              <News key="entertainment" pageSize={5} category="entertainment" country="in"/>
+          </Route>
+          <Route exact path="/health">
+              <News key="health" pageSize={5} category="health" country="in"/>
+          </Route>
+          <Route exact path="/general">
+              <News key="general" pageSize={5} category="general" country="in"/>
+          </Route>
+          <Route exact path="/science">
+             <News key="science" pageSize={5} category="science" country="in"/>
+          </Route>
+          <Route exact path="/technology">
+              <News key="technology" pageSize={5} category="technology" country="in"/>
+          </Route>
+          <Route exact path="/sports">
+              <News key="sports" pageSize={5} category="sports" country="in"/>
+          </Route>
+        </Switch>
+
+
+        </Router>
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+// business
+// entertainment
+// general
+// health
+// science
+// sports
+// technology
